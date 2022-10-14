7b06a088318b638a645cc15b94761d72c167fb2c
IgorGazineo <gaumgazineo@hotmail.com>
2022-10-14 12:24:03 -0300

    atualização 01

--- exercicios/ex01/scriptex1.js
+++ exercicios/ex01/scriptex1.js
@@ -2,4 +2,4 @@ var valorP = Number(prompt('Digite o valor do seu produto:'))
 var desconto = Number(prompt('Digite (apenas com números) o desconto do produto em porcentagem:'))
     descontoFinal = desconto * valorP/100
     valorFinal = valorP - descontoFinal
-alert(`O produto de valor: ${valorP} reais com ${desconto}% de deconto, sai por ${valorFinal} reais.  `)
+alert(`O produto de valor: ${valorP} reais com ${desconto}% de deconto (${descontoFinal} reais), sai por ${valorFinal} reais.  `)
